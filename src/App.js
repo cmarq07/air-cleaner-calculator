09b95418d6160ab3ff8ad6f7135ccde95fc4ace6
Misty Becker <mistyb01@uw.edu>
2022-02-26 23:09:34 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,6 +19,23 @@ function App() {
         </a>
       </header>
     </div>
+=======
+import React from 'react';
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom';
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
+import Routes from './Routes.js';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes />
+      <Footer />
+    </Router>
+>>>>>>> Stashed changes
   );
 }
 
