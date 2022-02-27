df3faa2f151c9ccad5c7e867591e50408dfca3c2
Misty Becker <mistyb01@uw.edu>
2022-02-26 23:40:17 -0800

    hamburger menu changes to 'x' when clicked

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,14 +8,14 @@ export function Header() {
                     <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
                 </a>
 
-                <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <a role="button" className="navbar-burger" is-active="true" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                 </a>
             </div>
 
-            <div id="navbarBasicExample" className="navbar-menu">
+            <div id="navbar-menu" className="navbar-menu" is-active="true">
                 <div className="navbar-start">
                     <a className="navbar-item">
                         Home
@@ -63,7 +63,36 @@ export function Header() {
             </div>
         </nav>
     );
+
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    console.log($navbarBurgers.length);
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+
+        // Add a click event on each of them
+        $navbarBurgers.forEach(el => {
+            el.addEventListener('click', () => {
+                // Get the target from the "data-target" attribute
+                const target = el.dataset.target;
+                const $target = document.getElementById(target);
+                console.log("test");
+
+                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+
+
+            });
+        });
+    }
+
+});
 
 export default Header;
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -11,10 +11,17 @@ export function Home() {
                         <div className="box p-6">
                             <div className="buttons are-medium is-centered">
                                 <Link to="/formlanding">
-                                    <button className="button is-primary">Find or test a portable air cleaner.</button>
+                                    <button className="button is-info">Find or test a portable air cleaner.</button>
                                 </Link>
                             </div>
                         </div>
+
+                        <section className="hero is-info is-small">
+                            <div className="hero-body">
+                                <p>Before you find or test your air cleaner, learn more about air filtration and best practices!</p>
+                                <button className="button is-info is-light m-6">Take our Air Care Crash Course.</button>
+                            </div>
+                        </section>
                     </div>
                 </div>
             </section>
