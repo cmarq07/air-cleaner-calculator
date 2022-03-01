0c4d2d5c1993c5645d55ee93948c792a32885470
Misty Becker <mistyb01@uw.edu>
2022-02-28 16:11:47 -0800

    hamnburger icon expands navigation links

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,13 @@ import '../App.css';
 
 export function Header() {
     return (
-        <nav className="navbar" role="navigation" aria-label="main navigation">
+        <nav className="navbar mx-3" role="navigation" aria-label="main navigation">
             <div className="navbar-brand">
                 <a className="navbar-item" href="https://bulma.io">
                     <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
                 </a>
 
-                <a role="button" className="navbar-burger" is-active="true" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <a role="button" id="burger" className="navbar-burger" is-active="true" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
@@ -16,50 +16,20 @@ export function Header() {
             </div>
 
             <div id="navbar-menu" className="navbar-menu" is-active="true">
-                <div className="navbar-start">
+                <div className="navbar-end">
                     <a className="navbar-item">
                         Home
                     </a>
 
                     <a className="navbar-item">
-                        Documentation
+                        Resources
                     </a>
 
-                    <div className="navbar-item has-dropdown is-hoverable">
-                        <a className="navbar-link">
-                            More
-                        </a>
-
-                        <div className="navbar-dropdown">
-                            <a className="navbar-item">
-                                About
-                            </a>
-                            <a className="navbar-item">
-                                Jobs
-                            </a>
-                            <a className="navbar-item">
-                                Contact
-                            </a>
-                            <hr className="navbar-divider" />
-                            <a className="navbar-item">
-                                Report an issue
-                            </a>
-                        </div>
-                    </div>
+                    <a className="navbar-item">
+                        About
+                    </a>
                 </div>
 
-                <div className="navbar-end">
-                    <div className="navbar-item">
-                        <div className="buttons">
-                            <a className="button is-primary">
-                                <strong>Sign up</strong>
-                            </a>
-                            <a className="button is-light">
-                                Log in
-                            </a>
-                        </div>
-                    </div>
-                </div>
             </div>
         </nav>
     );
@@ -81,16 +51,24 @@ document.addEventListener('DOMContentLoaded', () => {
                 // Get the target from the "data-target" attribute
                 const target = el.dataset.target;
                 const $target = document.getElementById(target);
-                console.log("test");
 
                 // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
                 el.classList.toggle('is-active');
                 $target.classList.toggle('is-active');
 
 
+
             });
         });
     }
+
+
+    const burgerIcon = document.querySelector('#burger');
+    const navBarMenu = document.querySelector('#navbar-menu');
+
+    burgerIcon.addEventListener('click', () => {
+        navBarMenu.classList.toggle('is-active');
+    });
 
 });
 
