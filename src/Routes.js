a05ce7b841d64e2bf5a90f21b9441620421fe56e
stanleysusanto <stanleysusanto8@email.com>
2022-03-01 16:25:25 -0800

    Form3-template

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from './components/Home.js';
 import FormLanding from './components/FormLanding.js';
+import Form3 from './components/Form3';
 
 export function RouteUrl() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/formlanding" element={<FormLanding />} />
+            <Route path="/formlanding" element={<Form3 />} />
         </Routes>
     );
 }
