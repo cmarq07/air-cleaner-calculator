53bc7a2a449d4fcca31eaf95caae8a0dacd4df16
Christian Calloway <49878807+cmarq07@users.noreply.github.com>
2022-03-03 22:19:45 -0800

    Update Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from './components/Home.js';
 import FormLanding from './components/FormLanding.js';
-import Form3 from './components/Form3';
 
 export function RouteUrl() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/formlanding" element={<FormLanding />} />
-            <Route path="/form3" element={<Form3 />} />
         </Routes>
     );
 }
