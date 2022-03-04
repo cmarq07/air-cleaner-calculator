5ed51a085cf4f77589395a231213349a3a11055c
natthapatsakulborrirug <nattts@uw.edu>
2022-03-03 20:29:51 -0800

    added dummy data

--- src/components/FormFour.js
+++ src/components/FormFour.js
@@ -1,7 +1,25 @@
 import '../App.css';
 import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Col, Form, FormGroup, Label, Input } from 'reactstrap';
+import data from '../air_cleaner_list.csv';
+
 
 export function FormFour() {
+
+    
+    function createSelectItems(airCleaners) {
+        let items = [];  
+        items.push(<option></option>);
+        for (let i = 0; i <= airCleaners.length - 2; i++) {   
+            let cadr = airCleaners[i].cadr;
+            let name = airCleaners[i].name;      
+            items.push(<option value={cadr}>{name}</option>);   
+        }
+        items.push(<option value={0}>I'm not sure</option>);
+        return items;
+    }
+
     return (
         <div className="p-6">
            <div>
@@ -15,23 +33,40 @@ export function FormFour() {
 
            <div className="card p-4">
                <p><strong>Tell us about your air cleaner.</strong></p>
-               <p>Air Cleaner Model Name</p>
+            <div class="field">
+                <p>Air Cleaner Model Name</p>
+                <div class="control">
+                    <div class="select">
+                    <select>
+                        <option value="DEFAULT">Select Model Name...</option>
+                        <option>Levoit Vital 100 True HEPA Air Purifier	</option>
+                        <option>Whirlpool® WPT80 Whispure™ Large Tower Air Purifier	</option>
+                        <option>Oransi OV200 Air Purifier	</option>
+                        <option>Coway Airmega 150	</option>
+                        <option>BioGS 2.0 Ultra Quiet Air Purifier SPA- 550A	</option>
+                        <option>Honeywell True HEPA Large Room Air Purifier With Allergen Remover	</option>
+                        <option>MinusA2 Ultra Quiet Air Purifier SPA-780	</option>
+                        <option>MinusA2 Ultra Quiet Air Purifier SPA- 700	</option>
+                        <option>Coway Airmega AP-1512HH	</option>
+                        <option>LV-H134 Tower Pro True HEPA Air Purifier	</option>
+                    </select>
+                    </div>
+                </div>
+            </div>
 
-               <select className="user-input" id="air-cleaner-selection" data-tip data-for="cadr-model-tooltip" onChange={(e) => {
-                            
-                    }}>
-                </select>
-
-               <p>Number of Air Cleaners in this Space</p>
-               <span>
-                    <input className="mr-2" type="number" id="numAirCleanerInput" />
-                    <span className="variableInput"></span>
-
-                </span>
+            <div class="field">
+                <p>Number of Air Cleaners in this Space</p>
+                <div class="control">
+                    <input class="input" type="number" placeholder="Enter Number of Air Cleaners"></input>
+                </div>
+            </div>
             </div>
         
+
+
+
             <div class="buttons is-centered">
-                <button class="button is-primary mt-6 has-text-centered">See Results</button>
+                <button class="button is-info mt-6 has-text-centered">See Results</button>
             </div>
         </div>
     )
