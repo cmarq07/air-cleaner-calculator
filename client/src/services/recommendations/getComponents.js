9f7df18153ba5347ace45f745a567bf86c46e9db
Misty Becker <mistyb01@uw.edu>
2022-04-30 15:03:44 -0700

    added google search query button to recommendations page

--- client/src/services/recommendations/getComponents.js
+++ client/src/services/recommendations/getComponents.js
@@ -1,58 +1,47 @@
 export default function getComponents(data, isMobile) {
-    // Air Cleaner Alt Text
-    let airCleanerAlt
+
 
     let airCleanerComponents = data.map((airCleaner) => {
-        if (isMobile) {
-            return (
-                <div className='box mb-3 p-3 is-flex is-flex-direction-row is-justify-content-space-between' key={airCleaner.id}>
-                    {/* Air Cleaner Image */}
-                    <div className='image is-128x128 has-background-white-ter is-align-self-center imageItem'>
-                        <img src={airCleaner.image} alt={airCleanerAlt ? airCleanerAlt : "No Image"} className='image is-128x128'></img>
-                    </div>
+        let airCleanerAlt = "image of " + airCleaner.name
+        let searchQuery = ""
+        let nameForQuery = airCleaner.name.split(" ")
 
-                    {/* Air Cleaner Text */}
-                    <div className='m-2 s-justify-content-flex-start is-flex-grow-1'>
-                        <h1>{airCleaner.name}</h1>
-                        <h2>You'd need <strong className='has-text-info-dark'>{airCleaner.num_needed}</strong> of these.</h2>
-                        <br />
-                        {/*<strong className='has-text-info-dark'>{airCleaner.annual_kwh} kW/h</strong>*/}
-                        <br />
-                        <span>{airCleaner.cadr} CADR</span>
-                    </div>
+        for (let i = 0; i < nameForQuery.length; i++) {
+            searchQuery += nameForQuery[i];
+            if (i+1 != nameForQuery.length) {
+                searchQuery += "+"
+            }
+        }
 
-                    {/* Compare Button */}
-                    <div className='is-align-self-flex-end'>
-                    <a href={airCleaner.link}><button className='button is-info'>Store Info</button></a>
-                    </div>
+        let googleShoppingUrl = "https://www.google.com/search?tbm=shop&q="+searchQuery;
+
+
+        return (
+            <div className='box mb-3 p-3 is-flex is-flex-direction-row is-justify-content-space-between' key={airCleaner.id}>
+                {/* Air Cleaner Image */}
+                <div className='image is-128x128 has-background-white-ter is-align-self-center imageItem'>
+                    <img src={airCleaner.image} alt={airCleanerAlt ? airCleanerAlt : "No Image"} className='image is-128x128'></img>
                 </div>
-            )
-        } else {
-            return (
-                <div className='box m-3 p-3 is-flex is-flex-direction-row is-justify-content-space-between' key={airCleaner.id}>
-                    {/* Air Cleaner Image */}
-                    <div className='image is-128x128 has-background-white-ter is-align-self-center imageItem'>
-                        <img src={airCleaner.image}  alt={airCleanerAlt ? airCleanerAlt : "No Image"} className='image is-128x128'></img>
-                    </div>
 
-                    {/* Air Cleaner Text */}
-                    <div className='m-2 s-justify-content-flex-start is-flex-grow-1'>
-                        <h1>{airCleaner.name}</h1>
-                        <h2 className='has-text-info-dark'>Smoke CADR of {airCleaner.cadr}</h2>
-                        <br />
-                        <p>You would need of {airCleaner.num_needed} these to clean your entire space</p>
-                        {/*<span>Annual energy usage: <strong>{airCleaner.annual_kwh} kW/h</strong></span>*/}
-                        <br />
-                    </div>
-
-                    {/* Compare Button */}
-                    <div className='is-align-self-flex-end'>
-                        <a href={airCleaner.link}><button className='button is-info'>Store Info</button></a>
-                    </div>
+                {/* Air Cleaner Text */}
+                <div className='m-2 s-justify-content-flex-start is-flex-grow-1'>
+                    <strong>{airCleaner.name}</strong>
+                    <h2>You'd need <strong className='has-text-info-dark'>{airCleaner.num_needed}</strong> of these.</h2>
+                    <br />
+                    {/*<strong className='has-text-info-dark'>{airCleaner.annual_kwh} kW/h</strong>*/}
+                    <br />
+                    <span>{airCleaner.cadr} CADR</span>
                 </div>
+
+                {/* Compare Button */}
+                <div className='shopButtons'>
+                <a href={airCleaner.link} target="_blank"><button className='button is-info'>Store Info</button></a>
+                <a href={googleShoppingUrl} target="_blank"><button className='button is-info'>Search Stores</button></a>
+                </div>
+            </div>
             )
         }
-    })
+    );
 
     return airCleanerComponents.slice(0, 10)
 }
