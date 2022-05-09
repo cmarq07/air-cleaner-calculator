a85d56587bf71ee849916007f14df0d6d1d1d783
Bijou Kim <bijoukim0805@gmail.com>
2022-05-09 11:56:23 -0700

    home page

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,12 +1,10 @@
 import '../styles/styles.css';
-
+import iSchool from '../images/iSchool.png'
 export function Footer() {
     return (
         <footer className="footer">
-            <div className="content has-text-centered">
-                <p>
-                    <a href="">Contact Us</a>
-                </p>
+            <div className="footer-div">
+                <img src={iSchool} alt='A graphic of the information school logo'/>
             </div>
         </footer>
     );
