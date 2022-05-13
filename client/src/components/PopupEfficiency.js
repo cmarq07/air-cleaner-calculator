e0ee63195bc0d19997ca1d9c12d79e9e15aa96e4
Misty Becker <mistyb01@uw.edu>
2022-05-13 13:01:22 -0700

    pop ups size correctly, blurry image replaced

--- client/src/components/PopupEfficiency.js
+++ client/src/components/PopupEfficiency.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
-import ResultsPopupGraphic from '../images/results_popup_graphic.png'
+import ResultsPopupGraphic from '../images/results_popup_graphic.svg'
 
 //
 
@@ -11,19 +11,14 @@ export function PopupEfficiency() {
   return (
     
 
-      <Popup open={open} closeOnDocumentClick onClose={closeModal}>
-        <div className="info-modal">
-          <div className="content">
-          <img src={ResultsPopupGraphic} width="50%" height="50%" alt="Results popup graphic" />
-          <br/>
-          <h3 className="header">How did we calculate this?</h3>
-          <p>We take your room volume, your air cleaner's CADR (clean air delivery rate), and an averaged outdoor ventilation number.</p>
-          <p>We run it through our algorithm to <b>make sure that you're achieving a level of four air changes per hour!</b></p>
-          <div className="button-container">
-            <button type="button" className="button is-info" onClick={closeModal}>View Results</button>
-          </div>
-          </div>
-        </div>
+      <Popup className="modal" open={open} closeOnDocumentClick onClose={closeModal}>
+            <img src={ResultsPopupGraphic} width="50%" height="50%" alt="Results popup graphic" />
+            <br/>
+            <h1 className="title is-4">How did we calculate this?</h1>
+            <p>We take the size of your room, and the strength of your air cleaner, then run it through our algorithm to <b>make sure that you're achieving a level of four air changes per hour!</b></p>
+            <div className="button-container">
+              <button type="button" className="button is-info" onClick={closeModal}>View Results</button>
+            </div>
       </Popup>
     
   );
--- client/src/components/PopupRecommendations.js
+++ client/src/components/PopupRecommendations.js
@@ -10,19 +10,15 @@ export function PopupRecommendations() {
   const closeModal = () => setOpen(false);
   return (
     
-
-      <Popup open={open} closeOnDocumentClick onClose={closeModal}>
-        <div className="info-modal">
-          <div className="content">
-          <img src={RecommendationsPopupGraphic} width="50%" height="50%" alt="Recommendations popup graphic"/>
-          <br/>
-          <h3 className="header">Here are the air cleaners that we recommend for your space!</h3>
-          <div className="button-container">
-            <button type="button" className="button is-light" onClick={closeModal}>Back to form</button>
-            <button type="button" className="button is-info" onClick={closeModal}>View List</button>
-          </div>
-          </div>
-        </div>
+    
+      <Popup className="modal" open={open} closeOnDocumentClick onClose={closeModal}>
+            <img src={RecommendationsPopupGraphic} width="50%" height="50%" alt="Recommendations popup graphic"/>
+            <br/>
+            <h3 className="header">Here are the air cleaners that we recommend for your space!</h3>
+            <div className="button-container">
+              <button type="button" className="button is-light" onClick={closeModal}>Back to form</button>
+              <button type="button" className="button is-info" onClick={closeModal}>View List</button>
+            </div>
       </Popup>
     
   );
